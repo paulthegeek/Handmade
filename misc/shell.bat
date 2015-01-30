7769ecd5eb8b374147b33336355fec3b4ae0ea5d
Paul Jackson <pj2454@gmail.com>
2015-01-30 00:38:36 -0700

    Updated shell so it compliler is ready on open

--- misc/shell.bat
+++ misc/shell.bat
@@ -1,2 +1,2 @@
 @echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\vc\vcvarsall.bat" x64
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x64
--- shell.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x64
