d7c201c0dfd201f999312213ca8238596faacf85
Paul Jackson <pj2454@gmail.com>
2015-02-01 00:35:46 -0700

    can open editor and build in editor

---
+++ misc/edit.bat
@@ -0,0 +1,3 @@
+@echo off
+
+start /d "C:\Program Files (x86)\Sublime Text 3" sublime_text.exe
--- misc/shell.bat
+++ misc/shell.bat
@@ -1,2 +1,6 @@
 @echo off
+
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x64
+doskey l=dir
+doskey vs=devenv w:\build\win32_handmade.exe
+set path=w:\handmade\misc;%path%
